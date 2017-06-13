7ea513cf20498714a9639947094cfed689ddee61
zengrd <zrda27@163.com>
2017-06-13 18:11:22 +0800

    sf

--- rh-auth-server/src/main/java/com/cmrh/auth/config/WebSecurityConfig.java
+++ rh-auth-server/src/main/java/com/cmrh/auth/config/WebSecurityConfig.java
@@ -1,42 +1,31 @@
 package com.cmrh.auth.config;
 
-import javax.servlet.Filter;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private Filter ssoFilter;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/**/*.html").permitAll()	//不拦截/resources/static/目录下的资源
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()	//使用表单登录
-				.loginPage("/login.html")
-				.defaultSuccessUrl("/index.html")
-				.permitAll()
-				.and()
-			.httpBasic()
-				.and()
-			.logout()
-				.logoutUrl("/logout")
-				.logoutSuccessUrl("/index.html")
-				.invalidateHttpSession(true)
-				.and()
-			.addFilterBefore(ssoFilter, BasicAuthenticationFilter.class);
-	}
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .antMatcher("/**").authorizeRequests()
+            .antMatchers("/", "/login**", "/webjars/**").permitAll()
+            .anyRequest().authenticated()
+        .and()
+            .formLogin()
+        .and();
+        // @formatter:on
+    }
 	
 	@Bean
 	@Override
